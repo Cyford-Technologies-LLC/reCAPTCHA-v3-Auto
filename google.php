a75d994a5876b78281d4bf163510e2b09c25ed25
Allen Ford <allen@cyfordtechnologies.com>
2024-12-26 00:28:25 -0500

    Update google.php

--- google.php
+++ google.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
+// And google details
 $secret = 'sxtkxtktk26d6HHXHxt2h667sxs20x62';
 $site_key = 'dxshtxdthnnxfbn6x64gnxgnYJDazrfgxbh';
 
